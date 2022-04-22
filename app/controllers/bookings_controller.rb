ec74edbd67c8f436aa30c3a83d2f67773800feab
yoann touffet <yoann.touffet@gmail.com>
2022-04-22 18:57:06 +0200

    controllers enc ours

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,21 +3,45 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    def create
+      @booking = Booking.new(booking_params)
+      @car = Car.find(params[:car_id])
+      @booking.car = @car
+      @booking.user = current_user
+
+      if @booking.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
   end
 
   def destroy
+    @booking.destroy
+    redirect_to root_path, notice: 'Booking successfully destroyed.'
+    authorize @booking
   end
 
   def update
+    @booking.available = false
+    @booking.save
+    redirect_to root_path
+    authorize @booking
   end
 
   def show
+    authorize @booking
   end
 
   def edit
+    authorize @booking
   end
+
+  private
 
 end
